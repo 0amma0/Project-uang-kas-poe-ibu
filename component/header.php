396f78fe4e64b8d412294b4546c60d9285446ab5
Sabbilarasyad <rasyabilaa14@gmail.com>
2026-04-05 14:00:48 +0700

    dashboard baru

--- component/header.php
+++ component/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="id">
 <head>
-  <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/dashboard.css">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <!-- Navbar -->
