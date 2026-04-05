d0ee798c250f43f1aedf76ce275979afd8d63524
Sabbilarasyad <rasyabilaa14@gmail.com>
2026-04-05 14:05:35 +0700

    refactor struktur index

--- page/dashboard.php
+++ page/dashboard.php
@@ -23,6 +23,8 @@ $features = [
 ];
 ?>
 
+<link rel="stylesheet" href="assets/css/dashboard.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">    
 <!-- HERO -->
 <section class="hero">
 <div class="hero-container">
